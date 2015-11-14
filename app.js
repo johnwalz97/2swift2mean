a9fafd8e02aee2c72fbfb208ff0d3a0ecb128ed0
John Walz <johnwalz97@gmail.com>
2015-11-14 00:01:31 +0000

    update

--- app.js
+++ app.js
@@ -15,12 +15,12 @@ var Schema = mongoose.Schema;
 
 var TeamSchema = new mongoose.Schema({
 	name: {type: String, required: true},
-    members: [{type: Schema.Types.ObjectId, ref: 'Comments'}],
+    members: [{type: Schema.Types.ObjectId, ref: 'Student'}],
 	created_at: { type:Date, default: Date.now },
 	updated_at: { type:Date, default: Date.now }
 })
 var StudentSchema = new mongoose.Schema({
-    _post: {type: Schema.Types.ObjectId, ref: 'Post'},
+    _team: {type: Schema.Types.ObjectId, ref: 'Team'},
 	name: {type: String, required: true},
 	created_at: { type:Date, default: Date.now },
 	updated_at: { type:Date, default: Date.now }
@@ -46,7 +46,7 @@ app.get("/", function(req, res){
     })
 })
 app.get("/teams/:id", function(req, res){
-    Team.findOne({_id: req.params.id}).populate('students').exec(function(err, team){
+    Team.findOne({_id: req.params.id}).populate('members').exec(function(err, team){
         if (err) {
             console.log(err);
         } else {
@@ -65,9 +65,27 @@ app.get("/students", function(req, res){
         } else {
             console.log("No Errors");
         }
-        res.render("index", {
+        res.render("students", {
             students: students
         })
+    })
+})
+app.get("/students/:id", function(req, res){
+    Student.findOne({_id: req.params.id}, function(err, student){
+	Team.find({}, function(err, teams){
+	Team.findOne({_id: student._team}, function(err, team){
+        	if (err) {
+            		console.log(err);
+        	} else {
+            		console.log("No Errors");
+        	}
+        	res.render("show_student", {
+            		student: student,
+			teams: teams,
+			team: team
+        	})
+	    })
+	})
     })
 })
 
@@ -92,41 +110,62 @@ app.post("/students", function(req, res){
         if (err) {
             console.log(err);
         } else {
-            res.redirect('/');
+            res.redirect('/students');
         }
     })
 })
-app.post("/:student_id/:id", function(req, res){
-    Team.findOne({_id: req.params.id}, function(err, team){
-        Student.findOne({_id: req.params.student_id}, function(err, student){
-          student._team = team._id;
-          team.students.push(student);
+app.get("/:student_id/clear", function(req, res){
+    Student.findOne({_id: req.params.student_id}, function(err, student){
+        Team.findOne({_id: student._team}, function(err, team){
+          for(var i = 0; i < team.members.length; i++){
+		if(team.members[i] == student._team){
+			team.members = team.members.splice(i, 1);
+		}
+	  }
+	  student._team = null;
           student.save(function(err){
               team.save(function(err){
                   if (err) {
                       console.log(err);
                   } else {
-                      res.redirect('/');
+                      res.redirect('/students/'+req.params.student_id);
                   }
               })
           })         
         })
     })
 })
-app.get("/team/:id/destroy", function(){
-    Team.remove({_id: require.params.id}, function(err){
+app.get("/:student_id/:id", function(req, res){
+    Team.findOne({_id: req.params.id}, function(err, team){
+        Student.findOne({_id: req.params.student_id}, function(err, student){
+          student._team = team._id;
+          team.members.push(student);
+          student.save(function(err){
+              team.save(function(err){
+                  if (err) {
+                      console.log(err);
+                  } else {
+                      res.redirect('/students/'+req.params.student_id);
+                  }
+              })
+          })         
+        })
+    })
+})
+app.get("/team/:id/destroy", function(req, res){
+    Team.remove({_id: req.params.id}, function(err){
         if (err) {
             console.log(err);
         }
         res.redirect("/");
     })
 })
-app.get("/student/:id/destroy", function(){
-    Student.remove({_id: require.params.id}, function(err){
+app.get("/student/:id/destroy", function(req, res){
+    Student.remove({_id: req.params.id}, function(err){
         if (err) {
             console.log(err);
         }
-        res.redirect("/");
+        res.redirect("/students");
     })
 })
 
